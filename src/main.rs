caaa9d60337a9f4b6e59af65578f0246b22f0cfb
primal <primal@primal.host>
2026-02-17 17:49:45 -0500

    set total supply to 1 gKoi, gift 20 eKoi to each wallet

--- src/main.rs
+++ src/main.rs
@@ -17,8 +17,8 @@ use tokio::sync::{broadcast, Mutex};
 const RATE: f64 = 0.287_682_072_449_862; // ln(4/3)
 const PRATE: f64 = RATE * 10.0; // pending rate
 const SPY: f64 = 365.25 * 24.0 * 3600.0; // seconds per year
-const TOTAL_SUPPLY: f64 = 9_999_999_999.999_999_999;
-const GIFT: f64 = 100_000.0;
+const TOTAL_SUPPLY: f64 = 1_000_000_000.0;
+const GIFT: f64 = 20_000_000.0;
 
 fn now() -> f64 {
     SystemTime::now()
