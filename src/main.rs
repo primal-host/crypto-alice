188327721951b13e1bf57f87c22c66254d2bd316
primal <primal@primal.host>
2026-02-17 17:10:22 -0500

    set total supply to postgres bigint max (2^63-1)

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ const RATE: f64 = 0.287_682_072_449_862; // ln(4/3)
 const PRATE: f64 = RATE * 10.0; // pending rate
 const KPRATE: f64 = RATE / 10.0; // koi pending rate
 const SPY: f64 = 365.25 * 24.0 * 3600.0; // seconds per year
-const TOTAL_SUPPLY: f64 = 1_000_000_000_000_000.0;
+const TOTAL_SUPPLY: f64 = 9_223_372_036_854_775_807.0;
 const GIFT: f64 = 100_000.0;
 
 fn now() -> f64 {
